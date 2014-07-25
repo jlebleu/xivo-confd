d03b3911fe5b354a398b738cbf76e4406a2a3a70
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-25 13:55:46 -0400

    create user test unit

--- acceptance/tests/test_users.py
+++ acceptance/tests/test_users.py
@@ -1,6 +1,5 @@
 from test_api import scenarios as s
 from test_api.helpers import user as user_helper
-from test_api import client
 
 FIELDS = ['firstname',
           'lastname',
@@ -21,38 +20,13 @@ REQUIRED = ['firstname']
 BOGUS = [(f, 123, 'unicode string') for f in FIELDS]
 
 
-def test_errors_on_get():
-    scenarios = s.GetErrors("User",
-                            add_user,
-                            delete_user)
-    scenarios.run()
+class TestUserResource(s.GetScenarios, s.CreateScenarios, s.EditScenarios, s.DeleteScenarios):
 
+    url = "/users"
+    resource = "User"
+    required = REQUIRED
+    bogus_fields = BOGUS
 
-def test_errors_on_create():
-    scenarios = s.CreateErrors("/users",
-                               bogus_fields=BOGUS,
-                               required=REQUIRED)
-    scenarios.run()
-
-
-def test_errors_on_edit():
-    scenarios = s.EditErrors(add_user,
-                             delete_user,
-                             bogus_fields=BOGUS)
-    scenarios.run()
-
-
-def test_errors_on_delete():
-    scenarios = s.DeleteErrors("User",
-                               add_user,
-                               delete_user)
-    scenarios.run()
-
-
-def add_user():
-    user = user_helper.add_user(firstname='firstname')
-    return "/users/{}".format(user['id'])
-
-
-def delete_user(url):
-    client.delete(url)
+    def create_url(self):
+        user = user_helper.add_user(firstname='firstname')
+        return "/users/{}".format(user['id'])
