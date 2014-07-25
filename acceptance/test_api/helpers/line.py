e8c587dadc037e3c22c9448c2594cab77308e446
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-25 15:55:57 -0400

    add helper for generating a random line

--- acceptance/test_api/helpers/line.py
+++ acceptance/test_api/helpers/line.py
@@ -1,11 +1,11 @@
 from test_api import client
+from test_api import config
 from test_api.wrappers import IsolatedAction
 
 
 def add_line(**parameters):
     response = client.post("/lines_sip", parameters)
-    response.assert_status(201)
-    return response.json
+    return response.item
 
 
 def delete_line(line_id, check=False):
@@ -14,10 +14,14 @@ def delete_line(line_id, check=False):
         response.assert_status(204)
 
 
+def generate_line():
+    return add_line(context=config.CONTEXT, device_slot=1)
+
+
 class isolated_line(IsolatedAction):
 
     def __enter__(self):
-        self.line = add_line(context='default', device_slot=1)
+        self.line = generate_line()
         return self.line
 
     def __exit__(self, *args):
