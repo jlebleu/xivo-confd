341d9a74454c5300313b7cf502d220b7c868e2cf
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-28 16:48:18 -0400

    user url builder in line_extension tests

--- acceptance/tests/test_line_extension.py
+++ acceptance/tests/test_line_extension.py
@@ -2,16 +2,11 @@ from test_api.helpers.line import generate_line, delete_line
 from test_api.helpers.extension import generate_extension, delete_extension
 from test_api.scenarios import AssociationScenarios, DissociationScenarios, AssociationGetScenarios
 from test_api import assertions as a
-from test_api import client, url_for
+from test_api import restapi
 from test_api import fixtures
 import re
 import unittest
 
-
-list_url = url_for('line_extension.list')
-associate_url = url_for('line_extension.associate')
-dissociate_url = url_for('line_extension.dissociate')
-extension_line_url = url_for('extension_line.get')
 
 FAKE_ID = 999999999
 
@@ -21,7 +16,7 @@ already_associated_regex = re.compile(r"line with id \d+ already has an extensio
 not_associated_regex = re.compile(r"Line \(id=\d+\) is not associated with Extension \(id=\d+\)")
 
 
-class LineExtensionAssociation(object):
+class TestLineExtensionAssociation(AssociationScenarios, DissociationScenarios, AssociationGetScenarios):
 
     left_resource = "Line"
     right_resource = "Extension"
@@ -40,22 +35,13 @@ class LineExtensionAssociation(object):
         delete_extension(extension_id)
 
     def associate_resources(self, line_id, extension_id):
-        url = associate_url(line_id=line_id)
-        return client.post(url, extension_id=extension_id)
+        return restapi.lines(line_id).extensions.post(extension_id=extension_id)
 
     def dissociate_resources(self, line_id, extension_id):
-        url = dissociate_url(line_id=line_id, extension_id=extension_id)
-        return client.delete(url)
+        return restapi.lines(line_id).extensions(extension_id).delete()
 
     def get_association(self, line_id, extension_id):
-        url = list_url(line_id=line_id, extension_id=extension_id)
-        return client.get(url)
-
-
-class TestLineExtensionAssociation(LineExtensionAssociation, AssociationScenarios, DissociationScenarios, AssociationGetScenarios):
-
-    association_error_regex = already_associated_regex
-    not_associated_error_regex = not_associated_regex
+        return restapi.lines(line_id).extensions.get()
 
     @unittest.skip("will be fixed after refactoring DAO exceptions")
     def test_dissociation_when_right_does_not_exist(self):
@@ -65,16 +51,14 @@ class TestLineExtensionAssociation(LineExtensionAssociation, AssociationScenario
 @fixtures.line()
 @fixtures.extension()
 def test_get_line_from_extension_when_not_associated(line, extension):
-    url = extension_line_url(extension_id=extension['id'])
-    response = client.get(url)
+    response = restapi.extensions(extension['id']).line.get()
     a.assert_error(response, no_line_associated_regex)
 
 
 @fixtures.extension('from-extern')
 @fixtures.line()
 def test_associate_incall_to_line_without_user(incall, line):
-    url = associate_url(line_id=line['id'])
-    response = client.post(url, extension_id=incall['id'])
+    response = restapi.lines(line['id']).extensions.post(extension_id=incall['id'])
     a.assert_error(response, no_user_associated_regex)
 
 
@@ -82,10 +66,11 @@ def test_associate_incall_to_line_without_user(incall, line):
 @fixtures.extension()
 @fixtures.line()
 def test_associate_two_internal_extensions_to_same_line(first_extension, second_extension, line):
-    url = associate_url(line_id=line['id'])
-    response = client.post(url, extension_id=first_extension['id'])
+    associate = restapi.lines(line['id']).extensions
+
+    response = associate.post(extension_id=first_extension['id'])
     response.assert_status(201)
 
-    response = client.post(url, extension_id=second_extension['id'])
+    response = associate.post(extension_id=second_extension['id'])
     #we need to standardize status codes on these kinds of errors
     a.assert_error(response, already_associated_regex, 400)
