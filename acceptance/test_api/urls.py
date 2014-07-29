465c31c2f8d343ffc6fb597e3a6f17f6eda09a77
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-29 13:46:13 -0400

    refactor UrlFragment
    rename variables, add context manager

--- acceptance/test_api/urls.py
+++ acceptance/test_api/urls.py
@@ -1,37 +1,42 @@
 class UrlFragment(object):
 
     @classmethod
-    def new(cls, base=''):
+    def root(cls, base=''):
         return cls([base])
 
     def __init__(self, fragments):
         self.fragments = fragments
 
-    def __call__(self, value=None, param=None):
-        return self._build(self._add(value, param))
+    def __call__(self, fragment=None, p=None):
+        return self._build(self._add(fragment, p))
 
     def __getattr__(self, name):
         return self._build(self._add(name))
 
     def __str__(self):
-        full_url = '/'.join(self.fragments)
-        return full_url
+        return '/'.join(self.fragments)
 
     def __repr__(self):
-        return '<UrlFragment {} {}>'.format(str(self), self.fragments)
+        return '<{} {} {}>'.format(self.__class__.__name__, str(self), self.fragments)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
 
     def apply(self, **kwargs):
         fragments = [f.format(**kwargs) for f in self.fragments]
         return self._build(fragments)
 
     def _build(self, fragments):
-        return self.__class__(fragments)
+        return UrlFragment(fragments)
 
-    def _add(self, value=None, param=None):
-        if value and param:
-            raise ValueError("value and param are mutually exclusive")
-        if value:
-            return self.fragments + [str(value)]
-        if param:
-            return self.fragments + ['{{{}}}'.format(param)]
+    def _add(self, fragment=None, p=None):
+        if fragment and p:
+            raise ValueError("fragment and param are mutually exclusive")
+        if fragment:
+            return self.fragments + [str(fragment)]
+        if p:
+            return self.fragments + ['{{{}}}'.format(p)]
         return self.fragments
