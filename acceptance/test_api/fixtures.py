cca6fa4eb7ba3c00c5a0d9c8f313517c6afdfde8
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-07-29 10:49:56 -0400

    add fixtures for device and voicemail

--- acceptance/test_api/fixtures.py
+++ acceptance/test_api/fixtures.py
@@ -1,9 +1,10 @@
-import config
 from wrappers import IsolatedAction
 
 from helpers.user import generate_user, delete_user
 from helpers.line import generate_line, delete_line
 from helpers.extension import generate_extension, delete_extension
+from helpers.device import generate_device, delete_device
+from helpers.voicemail import generate_voicemail, delete_voicemail
 
 
 class user(IsolatedAction):
@@ -24,3 +25,13 @@ class extension(IsolatedAction):
                'delete': delete_extension}
 
 
+class device(IsolatedAction):
+
+    actions = {'generate': generate_device,
+               'delete': delete_device}
+
+
+class voicemail(IsolatedAction):
+
+    actions = {'generate': generate_voicemail,
+               'delete': delete_voicemail}
